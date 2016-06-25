ead3ab98c7c0711acaef3ff7de2623f4fc8f4d04
kanrikamit <karnikmait46@gmail.com>
2016-06-25 14:22:05 +0530

    Refactoring

--- main.py
+++ main.py
@@ -13,18 +13,14 @@ def main(msg_to_post):
 
 def get_api(cfg):
     graph = facebook.GraphAPI(cfg['access_token'])
-    # Get page token to post as the page. You can skip
-    # the following if you want to post as yourself.
     resp = graph.get_object('me/accounts')
     page_access_token = None
     for page in resp['data']:
         if page['id'] == cfg['page_id']:
-          page_access_token = page['access_token']
+            page_access_token = page['access_token']
         graph = facebook.GraphAPI(page_access_token)
     return graph
-    # You can also skip the above if you get a page token:
-    # http://stackoverflow.com/questions/8231877/facebook-access-token-for-pages
-    # and make that long-lived token as in Step 3
+
 
 if __name__ == "__main__":
   main("Hello, World!")
