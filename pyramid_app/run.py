919aa57f62013e63596d9870168cef4a7c2a7fff
kanrikamit <karnikmait46@gmail.com>
2016-06-26 14:08:27 +0530

    start server

--- pyramid_app/run.py
+++ pyramid_app/run.py
@@ -2,6 +2,8 @@
 __author__ = 'karnikamit'
 import logging
 from wsgiref.simple_server import make_server
+from main import make_app
+from fb import post
 
 
 def start_server(pyramid_app, host='127.0.0.1', port=9000):
@@ -15,3 +17,7 @@ def start_server(pyramid_app, host='127.0.0.1', port=9000):
     logging.info('pyramid wsgi app running on {0}:{1}'.format(host, port))
     server = make_server(host, port, pyramid_app)
     return server.serve_forever()
+
+if __name__ == '__main__':
+    app = make_app(post)
+    start_server(app)
